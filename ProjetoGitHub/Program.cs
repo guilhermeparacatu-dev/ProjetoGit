274f677464e43741b61dc6855a6b5c890e915df4
Guilherme Paracatu <guilherme_ata@hotmail.com>
2025-07-29 07:32:20 -0300

    feat: Classe Produto

--- ProjetoGitHub/Program.cs
+++ ProjetoGitHub/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+var produto = new Produto();
+produto.Descricao = "Teclado";
+produto.Id = 1;
+
+Console.WriteLine($"Produto: {produto.Descricao}");
